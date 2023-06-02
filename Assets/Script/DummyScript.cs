ddcd561c02499644c6445ea91408c13bb22a8aa4
Jaikishore <jaikishore@dlearners.in>
2023-06-02 19:42:48 +0530

    initial push

---
+++ Assets/Script/DummyScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DummyScript : MonoBehaviour
+{
+    public Image image;
+
+    public void ColourChangeOnClick(){
+        image.color = Color.red;
+    }
+}
